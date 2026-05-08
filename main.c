378f3c72652b267060cec50c0b3009134445857d
nadaselmane95-crypto <nadaselmane95@gmail.com>
2026-05-08 20:36:34 +0100

    write the operations and menu by tmrw 11 :))

---
+++ main.c
@@ -0,0 +1,214 @@
+* ================================================================
+      ALGORITHMICS AND DYNAMIC DATA STRUCTURES LAB WORK   
+   ================================================================
+   Data struct used is singly linked list of paragraphs, each
+   paragraph is a singly linked list of words.
+ 
+   Realised by Selmane Nada & Houchati Abdelmoumene 
+   ================================================================
+   Submitted May 10th 2026
+   ================================================================ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// CONSTANT DEFINITONS: 
+#define MaxWord 64
+#define MaxLine BUFSIZ 
+#define MaxFileName 256
+#define MaxDocuments 10 
+
+
+//DATA STRUCTURE 
+// 1- WORD : ELEMENT IN A PARAGRAPH 
+
+typedef struct WordCell {
+    char Word[MaxWord];
+    struct WordCell *NextWord;
+} WordCell;
+
+//2- PARAGRAPH : ELEMENT IN A TEXT DOCUMENT
+
+typedef struct ParagraphCell {
+    int paraName; 
+    WordCell *words;
+    struct ParagraphCell *next; 
+    int WordCount;
+}ParagraphCell;
+
+//3-DOCUMENT : SET OF PARAGRAPHS
+
+typedef struct DocumentCell {
+    char filename[MaxFileName];
+    struct ParagraphCell *Paragraph; 
+    int numberof_para;
+} DocumentCell; 
+
+// ================================================================
+// Words ABSTRACT MACHINE 
+// CHECK IF THE WORD IS EXISTANT IN A SET 
+int ExistentWord(WordCell *head , const char *word) {
+    for (; head; head=head->NextWord);
+    if (strcmp(head->Word,word)==0) return 1;
+    return 0;
+}
+ // INSERT A WORD IN THE LIST 
+int InsertWord(WordCell **head, const char *word ) {
+    //if the word is already existant, no need to insert it
+    if (ExistentWord(*head,word)) return 0;
+    // insert the word in the linked list 
+    WordCell *newword= malloc(sizeof(WordCell));
+    //copy the word we want to insert into the word field 
+    strcmp(newword->Word, word);
+    // link it to the list 
+    newword->NextWord = *head;
+    *head = newword;
+    return 1;
+}
+// DELETE SET OF WORDS 
+void DeleteSet(WordCell *head) { 
+    while (head !=NULL)    {
+    WordCell *temp = head;
+    head=head->NextWord;
+    free(temp); 
+    }
+}
+
+// PRINT A SET OF WORDS 
+void PrintSet(WordCell *head) {
+    int cpt = 0; 
+    if (!head) {
+        printf("The set is empty");
+        return;
+    }
+    while (head != NULL) {
+        printf("%s", head->Word);
+        if (++cpt % 10 == 0) {
+            printf("\n");
+        }
+    }
+  if (cpt % 10 !=10 ){ printf("\n"); }
+     
+}
+
+
+//THE ABSTRACT MACHINE OF PARAGRAPHS 
+// 1 - Create A paragraph 
+ParagraphCell *CreateParagraph(int id) {
+    ParagraphCell *p = malloc(sizeof(ParagraphCell)); 
+    p->paraName = id;
+    p->words = NULL;
+    p->next = NULL;
+    p->WordCount = 0; 
+    return p; 
+}
+
+// 2 - Insert A Paragraph At the Queue: 
+void InsertParagraph(ParagraphCell **head , ParagraphCell *paragraph){
+if (*head = NULL) { 
+    *head= paragraph ;
+    return; 
+}
+ParagraphCell *currentpara = *head; 
+while (currentpara!=NULL) { 
+    currentpara = currentpara->next;
+}
+currentpara->next = paragraph; 
+}
+
+// 3 - Extract A Paragraph : 
+ParagraphCell *ExtractParagraph(ParagraphCell *head, int index) {
+    int i = 1; 
+    while (i !=index) {
+        if (head == NULL) return NULL;  // index too big — stop safely
+        head = head->next; 
+        i++;
+    }
+    return head; 
+}
+
+// 4 - Delete A Paragraph : 
+void DeleteParagraph(ParagraphCell *head) { 
+while (head != NULL) {
+    ParagraphCell *temp = head;
+    DeleteSet(head->words); 
+    head= head->next;
+    free(temp);
+}
+}
+
+// ================================================================
+// Text PreProcessing 
+//1 - normalize text : Removes Upper case and punctuation 
+void normalize_word(char *word) {
+    int len = strlen(word);
+    while (len > 0 && !isalnum(word[len - 1])) {
+        word[len - 1] = '\0';   // replace punctuation with end of string
+        len--;
+    }
+    int start = 0;
+    while (word[start] && !isalnum(word[start])) {
+        start++;                // skip punctuation characters
+    }
+    if (start > 0)
+        memmove(word, word + start, len - start + 1);  // shift left
+    for (int i = 0; word[i]; i++) {
+        word[i] = tolower(word[i]);
+    }
+}
+
+//2- Split a line into seperated Words and insert them in a paragraph: 
+
+void add_words(const char *line, ParagraphCell *para) {
+    char buf[MaxLine];
+    strcpy(buf, line);
+
+    char *word = strtok(buf, " \t\r\n");
+    int count = 0;
+ while (word != NULL && count < MaxLine) {
+    normalize_word(word);
+    if (strlen(word) > 1)
+        if (insert_word(&para->words, word))
+            para->WordCount ++;
+    word = strtok(NULL, " \t\r\n");
+    count++;
+ }
+}
+
+/*================================================================
+FILE LOADING : load a txt format file 
+================================================================*/
+
+int load_document(DocumentCell *doc, const char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    char line[200];
+
+    if (fp == NULL)
+        return 0;
+    doc->Paragraph = NULL;
+    doc->numberof_para = 0;
+
+    ParagraphCell *p = CreateParagraph(1);
+    while (fgets(line, sizeof(line), fp))
+    {
+        if (strcmp(line, "\n") == 0)
+        {
+            InsertParagraph(&doc->Paragraph, p);
+            doc->numberof_para++;
+            p = CreateParagraph(doc->numberof_para + 1);
+        }
+        else
+        {
+            add_words(line, p);
+        }
+    }
+    InsertParagraph(&doc->Paragraph, p);
+    doc->numberof_para ++;
+    fclose(fp);
+    return 1;
+}
+
+//SET OPERATIONS 
